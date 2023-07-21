58853e300b9c749ca575da808aeae00edbace56b
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2023-07-21 03:17:59 -0600

    feat: adding under construction message

---
+++ app/about/layout.tsx
@@ -0,0 +1,14 @@
+export default function AboutLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        About layout
+        {children}
+      </body>
+    </html>
+  )
+}
